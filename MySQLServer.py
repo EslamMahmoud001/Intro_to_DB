b1d0a8ac71ad9218a4db3f63ee0992ad968ea9a1
EslamMahmoud001 <Eslam.Mahmoud767@gmail.com>
2024-10-20 22:39:01 +0300

    Task 2

--- MySQLServer.py
+++ MySQLServer.py
@@ -11,8 +11,18 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 
 # Create the database library_management if it doesn't exist
-mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-print("Database 'alx_book_store' created successfully!")
+try: 
+    # Connect to the MySQL database
+    mydb = mysql.connector.connect(
+            host='localhost',
+            username='root',
+            password='EMH1@mysqlDB',
+            database='alx_book_store'
+            )
+except mysql.connector.Error:
+    mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+    print("Database 'alx_book_store' created successfully!")
+    
 
 # Select the database
 mycursor.execute("USE alx_book_store")
