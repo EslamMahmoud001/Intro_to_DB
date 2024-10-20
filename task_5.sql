14e4736c27fa311d217b476db8aaf8a181012788
EslamMahmoud001 <Eslam.Mahmoud767@gmail.com>
2024-10-20 22:57:43 +0300

    Task 6

---
+++ task_5.sql
@@ -0,0 +1,4 @@
+USE alx_book_store;
+CREATE TABLE IF NOT EXISTS customer(customer_id INT PRIMARY KEY, customer_name VARCHAR(215), email VARCHAR(215), address TEXT)
+INSERT INTO customer(customer_id, customer_name, email, address)
+VALUES(1,"Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave.")
